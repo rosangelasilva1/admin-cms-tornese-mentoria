24fd98d69bc4cc9a2c26a9a0b5890754521f05b0
rosangela <roliveirasv@gmail.com>
2021-07-29 21:13:12 -0300

    adicionando paginação

--- Controllers/API/ApiAdministradoresController.cs
+++ Controllers/API/ApiAdministradoresController.cs
@@ -3,7 +3,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using admin_cms.Models.Infraestrutura.Database;
+using admin_cms.Models.Dominio.Entidades;
+using admin_cms.Models.Dominio.Services;
 using System.Linq;
+using X.PagedList;
+
 
 
 namespace admin_cms.Controllers.API
@@ -17,15 +21,13 @@ namespace admin_cms.Controllers.API
         {
             _context = context;
         }
-        
-        
-        // GET: Administradores
+                
+        // GET All: Administradores
         [Route("/api/administradores.json")]
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
-
-            var adms = from adm in( await _context.Administradores.ToListAsync())
+            var adms = from adm in( await _context.Administradores.ToPagedListAsync(page,AdministradorService.ITENS_POR_PAGINA))
                 select new {
                     Id = adm.Id,
                     Nome = adm.Nome,
@@ -36,5 +38,44 @@ namespace admin_cms.Controllers.API
             return StatusCode(200,adms);
         }
 
+         // GET ById: Administradores
+        [Route("/api/administradores/{id}.json")]
+        [HttpGet]
+        public async Task<IActionResult> getById(int id)
+        {
+            Administrador adm = await _context.Administradores.FindAsync(id);
+            return StatusCode(200,adm);
+        }
+
+        // POST: Administradores
+        [Route("/api/administradores.json")]
+        [HttpPost]
+        public async Task<IActionResult> Criar([FromBody] Administrador administrador)
+        {
+            _context.Administradores.Add(administrador);
+            await _context.SaveChangesAsync();
+            return StatusCode(200,administrador);
+        }
+
+         // PUT: Administradores
+        [Route("/api/administradores/{id}.json")]
+        [HttpPut]
+        public async Task<IActionResult> Change([FromBody] Administrador administrador)
+        {
+            _context.Administradores.Update(administrador);
+            await _context.SaveChangesAsync();
+            return StatusCode(200,administrador);
+        }
+
+          // DELETE: Administradores
+        [Route("/api/administradores/{id}.json")]
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Administrador adm = await _context.Administradores.FindAsync(id);
+            _context.Administradores.Remove(adm);
+            await _context.SaveChangesAsync();
+            return StatusCode(200);
+        }
     }
 }
---
+++ Models/Dominio/Services/AdministradorServices.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace admin_cms.Models.Dominio.Services{
+
+    public class AdministradorService{
+        public const int ITENS_POR_PAGINA = 2;
+    }
+}
